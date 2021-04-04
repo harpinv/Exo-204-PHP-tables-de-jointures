b9404eb42901e9dbc7eaa162173e3b5eab40274f
Nux007 <007.nux@gmail.com>
2021-04-04 10:13:28 +0200

    Exo 205

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * 1 - Uniquement pour la pratique, reproduisez via mysql workbench le schémas proposé.
+ * 2 - Exportez le résultat de manière à créer les tables en base de données.
+ * 3 - Ajoutez des utilisateurs, des rôles et ajoutez des données dans la table user_role ( attention, au moins un utilisateur doit avoir deux rôles au moins ).
+ * 4 - A l'aide d'un simple print_r, afficher les rôles de chaque utilisateur.
+ * 5 - FIN !
+ */
